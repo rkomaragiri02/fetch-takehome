5cd5fa6d120aa178b7ffffe8c1e7c267bd8dd0be
Ritvik <youritvik02@gmail.com>
2025-01-23 16:38:36 -0500

    Update vite.config.ts

--- fetch-takehome/vite.config.ts
+++ fetch-takehome/vite.config.ts
@@ -1,6 +1,8 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
+import dotenv from "dotenv";
+dotenv.config();
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -14,7 +16,7 @@ export default defineConfig(({ mode }) => {
     },
     define: {
       "process.env": {},
-      __BASE_API_URL: JSON.stringify(env.VITE_BASE_URL),
+      __BASE_API_URL: process.env.VITE_BASE_URL,
     },
   };
 });
